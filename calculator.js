9cd2c227224483068af7e493cd5e2493e73906d6
DarthHamza <hamzamakia@gmail.com>
2021-01-10 16:42:35 +0300

    task completed

--- calculator.js
+++ calculator.js
@@ -1,3 +1,12 @@
 // Define the numbers and the operation here
+const prompt = require("prompt-sync")({ sigint: true });
 
-// Write if-statements here
+let num1 = parseInt(prompt("Give me the first number: "));
+let num2 = parseInt(prompt("Give me the second number: "));
+let op = prompt("+, -, *, /?");
+
+if (op === "+") console.log(num1 + num2);
+else if (op === "-") console.log(num1 - num2);
+else if (op === "*") console.log(num1 * num2);
+else if (op === "/") console.log(num1 / num2);
+else console.log("You provided an incorrect operation!");
